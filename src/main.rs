16095a68fb17f65351b68ee893b1cc2e853906e3
zhang yang <zhyang918@163.com>
2024-12-30 15:53:39 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
 #[allow(dead_code)]
 mod algo;
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+
 fn main() {
     println!("welcome to rust data struct and algorithm");
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("You guessed: {}", guess);
+
+        // 换成OK，err保证程序不崩溃
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
